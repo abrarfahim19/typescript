58336c8226592e847fcd707b5ce437417b849d27
abrarfahim19 <fahimpatwari19@gmail.com>
2022-07-23 05:56:33 -0400

    Basic to Medium TypeScript convention

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,33 @@
 "use strict";
-let age = 25;
-if (age > 50) {
-    age += 1;
+let phone = 123456789;
+let user_name = 'Abrar';
+let total_user_choices = [1, 2, 3];
+let user_choices = ["Machine Learning", "Mechatronics", "Developing"];
+total_user_choices.forEach(n => n.toString);
+user_choices.forEach(n => n.length);
+let tuple = [user_name, 123456789];
+tuple[0].concat("Fahim");
+tuple[1].toString;
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+;
+const mySize = Size.Medium;
+;
+const mySizes = 2;
+function incomeTax(earning, taxYear = 2022) {
+    if (earning > 50000 && taxYear < 2022) {
+        return earning * 1.3;
+    }
+    return earning * 1.2;
 }
+let employee = {
+    id: 0,
+    name: 'Abrar',
+    retire: (date) => console.log(date)
+};
+employee.retire;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,44 @@
-let age:number = 25;
-if (age>50){
-    age += 1;
+let phone: number = 123_456_789;
+let user_name: string = "Abrar";
+let total_user_choices: number[] = [1, 2, 3];
+let user_choices: string[] = ["Machine Learning", "Mechatronics", "Developing"];
+
+// Auto completion method for number and string array
+total_user_choices.forEach((n) => n.toString);
+user_choices.forEach((n) => n.length);
+
+// Auto completion with TUPLE (Don't use more than two)
+let tuple: [string, number] = [user_name, 123_456_789];
+tuple[0].concat("Fahim");
+tuple[1].toString;
+// The method of string and number will pop up in code completion.
+
+// Using Enum
+enum Size {
+  Small = 1,
+  Medium,
+  Large,
 }
+const mySize: Size = Size.Medium;
+
+// Optimezed Enum
+const enum Sizes {
+  Small = 1,
+  Medium,
+  Large,
+}
+const mySizes: Sizes = Sizes.Medium;
+
+// Using Functions
+function incomeTax(earning: number, taxYear: number = 2022): number {
+  // let x = 3;
+  if (earning > 50_000 && taxYear < 2022) return earning * 1.3;
+  return earning * 1.2;
+}
+
+// Using object
+let employee: { id: number; name: string; retire: (date: Date) => void } = {
+  id: 0,
+  name: "Abrar",
+  retire: (date) => console.log(date),
+};
