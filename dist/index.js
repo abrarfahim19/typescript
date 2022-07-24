921d0b6c95ff235986867a0563380397be6ea861
abrarfahim19 <fahimpatwari19@gmail.com>
2022-07-24 13:34:23 -0400

    Advance Type 1.0

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,10 @@
 "use strict";
 let phone = 123456789;
-let user_name = 'Abrar';
+let user_name = "Abrar";
 let total_user_choices = [1, 2, 3];
 let user_choices = ["Machine Learning", "Mechatronics", "Developing"];
-total_user_choices.forEach(n => n.toString);
-user_choices.forEach(n => n.length);
+total_user_choices.forEach((n) => n.toString);
+user_choices.forEach((n) => n.length);
 let tuple = [user_name, 123456789];
 tuple[0].concat("Fahim");
 tuple[1].toString;
@@ -14,20 +14,34 @@ var Size;
     Size[Size["Medium"] = 2] = "Medium";
     Size[Size["Large"] = 3] = "Large";
 })(Size || (Size = {}));
-;
 const mySize = Size.Medium;
-;
 const mySizes = 2;
 function incomeTax(earning, taxYear = 2022) {
-    if (earning > 50000 && taxYear < 2022) {
+    if (earning > 50000 && taxYear < 2022)
         return earning * 1.3;
-    }
     return earning * 1.2;
 }
 let employee = {
     id: 0,
-    name: 'Abrar',
+    name: "Abrar",
+    retire: (date) => console.log(date),
+};
+let employee1 = {
+    id: 1,
+    name: "Fahim",
     retire: (date) => console.log(date)
 };
-employee.retire;
+function kgToLb(weight) {
+    if (typeof weight == "string") {
+        return parseInt(weight) * 1.2;
+    }
+    else {
+        return weight * 1.2;
+    }
+}
+let textBox = {
+    drag: () => { },
+    resize: () => { }
+};
+let tShirtSize = 50;
 //# sourceMappingURL=index.js.map
